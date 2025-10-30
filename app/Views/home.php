47577427dd843357c89f681cba4e742db94abdbe
Paulo Araujo <paulo.araujo39@fatec.sp.gov.br>
2025-10-30 20:26:16 -0300

    pastas

---
+++ app/Views/home.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" 
+    href="/css/style.css">
+    <title>SEMPRE JUNTOS</title>
+</head>
+<body>
+
+    <div class="superinfo-bg">
+        <div class="superinfo">
+            <p>Seg - Sex - 08:00 às 18:00 sab - 08:00 às 11:00</p>
+            <a href="tel:+5514997263677">+55 14 99726-3677</a>
+            <p>Rua: José Altimari, 394, Jdm João Paulo - Mineiros do Tietê - SP</p>
+
+        </div>
+
+    </div>
+
+    <header class="menu-bg">
+        <div class="menu">
+            <div class="menu-logo">
+                <a href="#">Sempre Juntos</a>
+
+            </div>
+            <nav class="menu-nav">
+                <ul>
+                    <li><a href="#sobre">Sobre</a></li>
+                    <li><a href="#produtos">produtos</a></li>
+                    <li><a href="#preco">preço</a></li>
+                    <li><a href="#qualidade">qualidade</a></li>
+                    
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>
+
+    <h1 class="introducao">Sempre Juntos<br> amor, fé e esperança</h1>
+
+    <section class="sobre" id="sobre">
+        <div class="sobre-info">
+            <h1>Sobre</h1>
+            <p>Bem-vindo(a) à Sempre Juntos, um espaço dedicado à fé, à espiritualidade e ao fortalecimento da vida com Deus.
+                Nossa missão é oferecer artigos religiosos de qualidade, cuidadosamente selecionados para inspirar devoção, esperança e amor em cada momento do seu dia.
+
+            </p>
+            <p>Aqui você encontra imagens sacras, terços, escapulários, velas, livros e acessórios litúrgicos — tudo pensado para quem busca expressar e viver sua fé de forma autêntica e profunda.
+
+            </p>
+            <p>Mais do que uma loja, somos uma comunidade de fé, comprometida em espalhar mensagens de luz e espiritualidade. Cada produto que oferecemos carrega um propósito: aproximar corações de Deus e fortalecer a presença do sagrado nos lares.
+                 Sempre Juntos — Onde a fé se transforma em gesto, e o gesto em bênção.
+
+            </p>
+        </div>
+        <div class="sobre-img">
+            <img src="/img/sobre1.jpg" alt="Sobre 1">
+        </div>
+        <div class="sobre-img">
+            <img src="/img/sobre2.jpg" alt="Sobre 2">
+        </div>
+    </section>    
+        <section class="produtos" id="produtos">
+            <h1>Produtos</h1>
+            <div class="produtos-container">
+                <div class="produtos-item purple">
+                    <h2>Terços</h2>
+                    <img src="img/produtos1.jpg" alt="Produtos 1">
+                </div>
+                <div class="produtos-item pink">
+                    <h2>Livros</h2>
+                    <img src="img/produtos2.jpg" alt="Produtos 2">
+                </div>
+                <div class="produtos-item blue">
+                    <h2>Imagens</h2>
+                    <img src="img/produtos3.jpg" alt="Produtos 3">
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="preco" id="preco">
+            <div class="preco-item">
+                <h2>Terços</h2>
+                <span>a partir de <sup>R$</sup>19,90</span>
+
+           
+            <ul>
+                <li>contas de pedras </li>
+                <li>contas de resinas </li>
+                <li>personalizados </li>
+            </ul>
+            <a href="#">Comprar</a>
+
+            </div>
+            <div class="preco-item">
+                <h2>Livros</h2>
+                <span>a partir de <sup>R$</sup>39,90</span>
+
+           
+            <ul>
+                <li>Livros </li>
+                <li>Cadernos </li>
+                <li>Agendas </li>
+                <li>Bíblias </li>
+            </ul>
+            <a href="#">Comprar</a>
+
+            </div>
+            <div class="preco-item">
+                <h2>Imagens</h2>
+                <span>a partir de <sup>R$</sup>99,90</span>
+
+           
+            <ul>
+                <li>Imagens de gesso </li>
+                <li>Imagens de Resina </li>
+                <li>Imagens personalizados </li>
+            </ul>
+            <a href="#">Comprar</a>
+
+            </div>
+
+        </section>
+        <section class="qualidade" id="qualidade">
+            <div class="qualidade-item">
+                <h2>Terço</h2>
+                <p>Cada terço que oferecemos é mais do que um acessório religioso — é um símbolo de fé, devoção e proteção.
+                    Nossos terços são produzidos com materiais de alta qualidade, garantindo durabilidade, conforto ao toque e beleza em cada detalhe.</p>
+
+            </div>
+
+                <div class="qualidade-item">
+                <h2>Cadernos</h2>
+                <p>acreditamos que a fé merece ser vivida com beleza, respeito e qualidade.
+                    Por isso, cada um de nossos produtos é escolhido e produzido com cuidado, dedicação e amor, para refletir o verdadeiro valor do sagrado em cada detalhe.</p>
+
+            </div>
+
+            </div>
+            <div class="qualidade-item">
+                <h2>Imagens</h2>
+                <p>acreditamos que a fé merece ser vivida com beleza, respeito e qualidade.
+                    Por isso, cada um de nossos produtos é escolhido e produzido com cuidado, dedicação e amor, para refletir o verdadeiro valor do sagrado em cada detalhe.</p>
+
+            </div>
+             <div class="qualidade-item">
+                <h2>Terço</h2>
+                <p>Cada terço que oferecemos é mais do que um acessório religioso — é um símbolo de fé, devoção e proteção.
+                    Nossos terços são produzidos com materiais de alta qualidade, garantindo durabilidade, conforto ao toque e beleza em cada detalhe.</p>
+
+            </div>
+       
+                <div class="qualidade-item">
+                <h2>Cadernos</h2>
+                <p>acreditamos que a fé merece ser vivida com beleza, respeito e qualidade.
+                    Por isso, cada um de nossos produtos é escolhido e produzido com cuidado, dedicação e amor, para refletir o verdadeiro valor do sagrado em cada detalhe.</p>
+
+            </div>
+            <div class="qualidade-item">
+                <h2>Imagens</h2>
+                <p>acreditamos que a fé merece ser vivida com beleza, respeito e qualidade.
+                    Por isso, cada um de nossos produtos é escolhido e produzido com cuidado, dedicação e amor, para refletir o verdadeiro valor do sagrado em cada detalhe.</p>
+
+            </div>
+
+        </section>
+        <section class="newsletter">
+            <div  class="newsletter-info">
+                <h1>Newsletter</h1>
+                <p>Assine e fique por dentro das novidades
+
+                </p>
+
+            </div>
+            <form class="newsletter-form">
+                <input type="text" placeholder="Seu e-mail">
+                <button type="submit">Assinar</button>
+            </form>
+
+        </section>
+
+
+
+    <footer class="footer">
+        <p>Sempre Juntos Todos os direitos reservados.</p>
+
+    </footer>
+    
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+// Importa o autoload do Composer para carregar as rotas
+require __DIR__ . '/../vendor/autoload.php';
+
+// Obtem a URL do navegador
+$url= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+if ($url == "/") {
+ require __DIR__ . '/../app/Views/home.php';
+}
